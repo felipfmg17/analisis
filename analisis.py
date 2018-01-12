6079f994feb4bf3bea14a7834de14ebd5e798c0b
ssdespostrade <ssdespostrade@grupobmv.com.mx>
2018-01-12 17:03:58 -0600

    genetic working, not deterministic

--- analisis.py
+++ analisis.py
@@ -59,7 +59,7 @@ def calcEMA(vals,a):
 		series.append(s)
 	return series
 
-def sim(pri, fiat, a, mins, lbuy, lsell, fee):
+def sim(pri, fiat, fee, a, mins, lbuy, lsell):
     ema = EMA(a)
     dev = Ratio( [pri[0]]*mins )
     ofiat, cryp, sells, buys, fees = fiat, 0, 0, 0, 0
@@ -87,65 +87,7 @@ def sim(pri, fiat, a, mins, lbuy, lsell, fee):
     gain = (fiat-ofiat)/ofiat
     return gain
 
-
-
-
-def born():
-    gen = [0,0,0,0]
-    gen[0] = random.random()
-    gen[1] = random.randint(1,60*24*7)
-    gen[2] = random.random()
-    gen[3] = -random.random()
-    return gen
-
-def mutate(gen):
-    gen = gen[:]
-    ale = random.randint(0,3)
-    gen[ale] += gen[ale]/2 - random.randint(0,1)
-    if ale==1 and gen[1]==0:
-        gen[1] = 1
-    return gen
-
-def reproduce(g1,g2):
-    g = [0,0,0,0]
-    for i in range( len(g) ):
-        c = random.randint(0,2)
-        if c==0:
-            g[i] = g1[i]
-        elif c==1:
-            g[i] = g2[i]
-        else:
-            g[i] = (g1[i]+g2[i])/2
-    return g
-
-
-def fitness(gen):
-    return sim(pri,fiat, gen[0], gen[1], gen[2], gen[3], fee)
-
-
-def evolution(pri, fiat, fee):
-    n = 1000
-    pop = [ born() for i in range(n) ]
-
-    for i in range(100):
-        for j in range(n/10):
-            ind = random.randrange(n)
-            pop.append( mutate(pop[ind]) )
-            pop.append( born() )
-            a = random.randrante(n)
-            b = random.randrange(n)
-            pop.append( reproduce(pop[a],pop[b]) )
-        sorted(pop, key=fitness)
-        pop = pop[:n]
-
-
-
-
-
-
-
-
-def simugraph(prices, usd, alpha, minutes, limbuy, limsell, fee):
+def simugraph(prices, usd, fee, alpha, minutes, limbuy, limsell):
     ema = EMA(alpha)
     ema_sig = []
     por = Ratio( [prices[0]]*minutes )
@@ -202,10 +144,82 @@ def simugraph(prices, usd, alpha, minutes, limbuy, limsell, fee):
     return gain
 
 
+def born():
+    gen = [0,0,0,0]
+    gen[0] = random.random()
+    gen[1] = random.randint(1,60*24)
+    gen[2] = random.random()
+    gen[3] = -random.random()
+    return gen
+
+def mutate(gen):
+    gen = gen[:]
+    ale = random.randint(0,3)
+    gen[ale] += gen[ale]/2 - random.randint(0,1)*gen[ale]
+    nvo = born()
+    if ale==0:
+        if gen[ale]<0 or gen[ale]>1:
+            gen[ale] = nvo[ale]
+    elif ale==1:
+        gen[ale] = int(gen[ale])
+        if gen[ale]<0:
+            gen[ale] = nvo[ale]
+    elif ale==2:
+        if gen[ale]<0 :
+            gen[ale] = nvo[ale]
+    elif ale==3:
+        if gen[ale]>0 :
+            gen[ale] = nvo[ale]
+    return gen
+
+
+
+    return gen
+
+def reproduce(g1,g2):
+    g = [0,0,0,0]
+    for i in range( len(g) ):
+        g[i] = g1[i] if random.randrange(2)==0 else g2[i] 
+    return g
+
+params = []
+
+def fitness(gen):
+    p = params + gen
+    return sim(*p)
+
+def evolution(pri, fiat, fee, n):
+    global params
+    params = [pri,fiat,fee]
+    pop = [ born() for i in range(n) ]
+    for i in range(500):
+        for j in range(n//10):
+            ind = random.randrange(n)
+            pop.append( mutate(pop[ind]) )
+            pop.append( born() )
+            a = random.randrange(n)
+            b = random.randrange(n)
+            pop.append( reproduce(pop[a],pop[b]) )
+        pop = sorted(pop, key=fitness, reverse=True)
+        pop = pop[:n]
+        print(i)
+    return pop
+
+
+
+
+
+def genprice():
+    x = [100 * math.sin((4 * t / 1000 - 2) * math.pi) + 10 * math.sin((16 * t / 1000 - 8) * math.pi) for t in range(1000)]
+    y = [random.randint(-30, 30) + t + 1000 for t in x]
+    return y
+
 def test3():
     x = [100 * math.sin((4 * t / 100 - 2) * math.pi) + 10 * math.sin((16 * t / 100 - 8) * math.pi) for t in range(100)]
     y = [random.randint(-15, 15) + t + 200 for t in x]
-    sim(y,100,1,1,0.03,-0.03,0.0001)
+
+    y = genprice()
+    simugraph(y,100,0.0001,0.3,80,0.05,-0.05)
 
 def test():
 	x = [ 100*math.sin( (4*t/100-2)*math.pi )   +  10*math.sin( (16*t/100-8)*math.pi ) for t in range(100) ]
@@ -263,5 +277,19 @@ def test2():
 	# Show the plot
 	plt.show()
 
-test3()
+def test4():
+    prices = genprice()
+    params = [prices, 100, 0.0001]
+    pop_n = 300
+    par = params + [pop_n]
+    pop = evolution( *par )
+    p = pop[0]
+    print(p)
+    par = params + p
+    simugraph( *par )
+
+
+
+
+test4()
 
